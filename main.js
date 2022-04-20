d316d48d3775fbc547f833fe4011be43e7c67c10
ryanrodrigu <97711855+ryanrodrigu@users.noreply.github.com>
2022-04-19 20:27:20 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -21,13 +21,6 @@ function modelLoaded() {
     console.log('Model Loaded!');
 }
 
-function speak(){
-    var synth = window.speechSynthesis;
-    speak_data_1 = "The first prediction is" + prediction_1;
-    speak_data_2 = "And the second prediction is" + prediction_2;
-    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
-    synth.speak(utterThis);
-}
 
 function check ()
 {
@@ -42,33 +35,5 @@ function gotResult(error, results) {
         console.log(results);
         document.getElementById("result_emotion_name").innerHTML = results[0].label;
         document.getElementById("result_emotion_name2").innerHTML = results[1].label;
-        prediction_1 = results[0].label;
-        prediction_2 = results[1].label;
-        speak();
-        if(results[0].label == "happy")
-        {
-            document.getElementById("update_emoji").innerHTML = "&#128522;";
-        }
-        if(results[0].label == "sad")
-        {
-            document.getElementById("update_emoji").innerHTML = "&#128532;";
-        }
-        if(results[0].label == "angry")
-        {
-            document.getElementById("update_emoji").innerHTML = "&#128548;";
-        }
-
-        if(results[0].label == "happy")
-        {
-            document.getElementById("update_emoji2").innerHTML = "&#128522;";
-        }
-        if(results[0].label == "sad")
-        {
-            document.getElementById("update_emoji2").innerHTML = "&#128532;";
-        }
-        if(results[0].label == "angry")
-        {
-            document.getElementById("update_emoji2").innerHTML = "&#128548;";
-        }
-    }
-}
+    };
+};
